792bcf87e2eb9c60596e3146d3f3107bd8cceddd
unknown <mustafausakli_@hotmail.com>
2022-08-21 20:41:41 +0300

    modal

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { moneyFormat } from '../helpers'
 import BasketItem from './BasketItem'
+import { AiOutlineClose } from "react-icons/ai"
 
 const Basket = ({ basket, products, total, resetBasket, modalToogle }) => {
     return (
         <div className='modal'>
-            <div>
+            <button className='modal-close' onClick={modalToogle}>
+                <AiOutlineClose className='close-icon' />
+            </button>
+            <div className='modal-product'>
                 {basket.map((item, index) => (
                     <BasketItem key={index} item={item} product={products.find(p => p.id === item.id)} />
                 ))}
